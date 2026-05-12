81bfc386380873c3a39934b20199c3dbf7683297
hxyyy1 <143918486+hxyyy1@users.noreply.github.com>
2026-05-12 11:01:09 +0800

    Update arxiv_retriever.py

--- src/zotero_arxiv_daily/retriever/arxiv_retriever.py
+++ src/zotero_arxiv_daily/retriever/arxiv_retriever.py
@@ -113,7 +113,7 @@ class ArxivRetriever(BaseRetriever):
             raise ValueError("category must be specified for arxiv.")
 
     def _retrieve_raw_papers(self) -> list[ArxivResult]:
-        client = arxiv.Client(num_retries=10, delay_seconds=10)
+        client = arxiv.Client(num_retries=15, delay_seconds=60)
         query = '+'.join(self.config.source.arxiv.category)
         include_cross_list = self.config.source.arxiv.get("include_cross_list", False)
         # Get the latest paper from arxiv rss feed
